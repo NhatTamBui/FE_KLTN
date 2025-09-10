e50fbdd8b0f994fb03cc825336207891fa516936
NhatTamBui <nhattambui88@gmail.com>
2025-09-10 19:27:05 +0700

    fix content

--- src/app/client/login/login.component.ts
+++ src/app/client/login/login.component.ts
@@ -9,6 +9,13 @@ import {Component, EventEmitter, Output} from '@angular/core';
 
 })
 export class LoginComponent {
+
+  /*
+  * Ví dụ cái đường dẫn endpoint là: http://localhost:8080/api/user/login
+  * Thì m chỉ cần gọi đến bằng cách: http.post('/user/login') thôi
+  *
+  *
+  * */
   @Output() close = new EventEmitter();
   @Output() signIn = new EventEmitter();
   @Output() signUp = new EventEmitter();
