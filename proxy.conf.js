f19a83c45a6e8b4822d2ceafa4c0c2865d2cf935
BuiNhatTam <nhattambui88@gmail.com>
2025-09-13 20:35:30 +0700

    add proxy to call api have been deploy on sv

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,13 +1,24 @@
 const PROXY_CONFIG = [
   {
     context: [
-      "/api",
+      // "/api",
     ],
     target: "http://localhost:8080",
     secure: false,
     "changeOrigin": true,
     "logLevel": "debug",
     headers: {host: 'localhost'},
+    pathRewrite: { "^/api": "/api" }
+  },
+  {
+    context: [
+      "/api",
+    ],
+    target: "https://toeicute-38c2b32a7c77.herokuapp.com/",
+    secure: false,
+    "changeOrigin": true,
+    "logLevel": "debug",
+    headers: {host: 'toeicute-38c2b32a7c77.herokuapp.com'},
     pathRewrite: { "^/api": "/api" }
   }
 ];
--- src/app/client/navbar/navbar.component.ts
+++ src/app/client/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {BsModalService} from "ngx-bootstrap/modal";
 import {LoginComponent} from "../login/login.component";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-navbar',
@@ -10,10 +11,20 @@ import {LoginComponent} from "../login/login.component";
 export class NavbarComponent {
 
 
-  constructor(private bs: BsModalService) {
+  constructor(private bs: BsModalService, private http: HttpClient) {
   }
 
   openLogin() {
     this.bs.show(LoginComponent, {class: 'modal-lg modal-dialog-centered'});
+  }
+
+  test() {
+    this.http.get("/api/test")
+      .subscribe((res: any) => {
+        console.log(res);
+        if(res?.succes) {
+          alert('ket noi dc roi a');
+        }
+      });
   }
 }
