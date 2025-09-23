d56b10aed5f1f6d9108e8f822f12290609c45196
BuiNhatTam <nhattambui88@gmail.com>
2025-09-23 22:28:03 +0700

    profile modify

--- src/app/client/profile/change-password/change-password.component.ts
+++ src/app/client/profile/change-password/change-password.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.scss']
 })
-export class ChangePasswordComponent {
+export class ChangePasswordComponent implements OnInit {
 
+  password: HTMLInputElement | null = null;
+  confirmPassword: HTMLInputElement | null = null;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.password = document.getElementById("password") as HTMLInputElement;
+    this.confirmPassword = document.getElementById("confirmPassword") as HTMLInputElement;
+  }
+
+  validatePassword(): boolean {
+    if (this.password && this.confirmPassword && this.password.value !== this.confirmPassword.value) {
+      this.confirmPassword.setCustomValidity("Mật khẩu mới không trùng khớp. Vui lòng nhập lại");
+      return false;
+    } else {
+      if (this.confirmPassword) {
+        this.confirmPassword.setCustomValidity('');
+      }
+      return true;
+    }
+  }
 }
--- src/app/client/profile/update-profile/update-profile.component.ts
+++ src/app/client/profile/update-profile/update-profile.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ToastrService} from "ngx-toastr";
+import {HttpClient} from "@angular/common/http";
+import {GetHeaderService} from "../../../common/get-headers/get-header.service";
+import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-update-profile',
   templateUrl: './update-profile.component.html',
   styleUrls: ['./update-profile.component.scss']
 })
-export class UpdateProfileComponent {
+export class UpdateProfileComponent implements OnInit{
+  profileForm!: FormGroup;
+  currentUser: any;
 
+  constructor(private toast: ToastrService,
+              private formBuilder: FormBuilder,
+              private http: HttpClient,
+              private getHeaderService: GetHeaderService) {
+  }
+
+
+  ngOnInit(): void {
+
+
+    this.profileForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+
+    const headers = this.getHeaderService.getHeaderAuthentication();
+    this.http.get('/api/user/get-profile', {
+      headers
+    })
+      .subscribe((res: any) => {
+        if (res?.success) {
+          this.currentUser = res?.data;
+        }
+      })
+  }
+
+  onSubmit(): void {
+    if (this.currentUser) {
+      // Xử lý khi form được submit, gửi thông tin lên server hoặc xử lý theo nhu cầu của bạn
+      console.log('Updating user:', this.currentUser);
+      // Gọi API để cập nhật thông tin của currentUser
+      this.http.post('/api/user/update-profile', this.currentUser).subscribe((res: any) => {
+        if (res?.success) {
+          this.toast.success('Profile updated successfully');
+        } else {
+          this.toast.error('Failed to update profile');
+        }
+      });
+    }
+  }
 }
