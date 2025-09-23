177d569a284db7acc04eaf5c50804c3a02c9e514
BuiNhatTam <nhattambui88@gmail.com>
2025-09-23 22:25:04 +0700

    fix proxy

--- proxy.conf.js
+++ proxy.conf.js
@@ -3,10 +3,23 @@ const PROXY_CONFIG = [
     context: [
       "/api",
     ],
+    target: "http://localhost:8080",
+    secure: false,
+    "changeOrigin": true,
+    "logLevel": "debug",
+    headers: {host: 'localhost'},
+    pathRewrite: {"^/api": "/api"}
+  },
+  {
+    context: [
+      // "/api",
+    ],
     target: "https://toeicute-38c2b32a7c77.herokuapp.com/",
+    secure: false,
     "changeOrigin": true,
     "logLevel": "debug",
     headers: {host: 'toeicute-38c2b32a7c77.herokuapp.com'},
     pathRewrite: {"^/api": "/api"}
+  },
 ];
 module.exports = PROXY_CONFIG;
