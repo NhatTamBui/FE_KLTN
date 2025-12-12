ece8c30dc4a411fbae1714ca17db96d8657d3def
BuiNhatTam <nhattambui88@gmail.com>
2025-12-12 20:13:39 +0700

    fix deploy

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,20 +1,20 @@
 const PROXY_CONFIG = [
-  {
-    context: [
-      "/api",
-    ],
-    target: "http://localhost:8080",
-    secure: false,
-    "changeOrigin": true,
-    "logLevel": "debug",
-    headers: {host: 'localhost'},
-    pathRewrite: {"^/api": "/api"}
-  },
+  // {
+  //   context: [
+  //     "/api",
+  //   ],
+  //   target: "http://localhost:8080",
+  //   secure: false,
+  //   "changeOrigin": true,
+  //   "logLevel": "debug",
+  //   headers: {host: 'localhost'},
+  //   pathRewrite: {"^/api": "/api"}
+  // },
   {
     context: [
        // "/api",
     ],
-    target: "http://192.168.1.26:8080",
+    target: "http://3.0.93.123:8080",
     secure: false,
     "changeOrigin": true,
     "logLevel": "debug",
