4a09b6e8a889931a9d04b61d66718efaf5f7f1f2
OrionSDust <thienphong2071@gmail.com>
2025-12-12 23:08:28 +0700

    update environment configuration and refactor API endpoint in AuthService

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -2,8 +2,9 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {AuthService} from "./auth.service";
-import {BASE_URL_LOCAL, CONSTANT} from "./common/constant";
+import {CONSTANT} from "./common/constant";
 import {ProfileService} from './common/profile.service';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class AuthServiceService implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    let apiEndPoint = new URL(req.url, BASE_URL_LOCAL);
+    let apiEndPoint = new URL(req.url, environment.apiUrl);
     const token = this.authService.getToken();
     // handle if running on production
     if (this.profileService.isDevelopmentMode || req.url.includes('assets')) {
@@ -27,7 +28,7 @@ export class AuthServiceService implements HttpInterceptor {
       const captcha = document.cookie.split(';').find((item: string) => item.includes(CONSTANT.captcha));
       req = req.clone({
         url: apiEndPoint.href,
-        withCredentials: true,  // send cookie
+        withCredentials: true,
         setHeaders: {
           Authorization: `Bearer ${token}`,
           'Captcha': captcha ? captcha.split('=')[1] : ''
